6b1b8fe71a4cc9353e14975053768680c5220618
Tim Schäfer <tim.schaefer@ae.mpg.de>
2025-11-26 11:23:21 +0100

    FIX: rename frontend run script and change port to 3000

--- frontend/run_dev.bash
+++ frontend/run_dev.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Connect to http://localhost:8000 now, e.g., run 'firefox http://localhost:8000 &'"
-python3 -m http.server 8000
+echo "Connect to http://localhost:3000 now, e.g., run 'firefox http://localhost:3000 &'"
+python3 -m http.server 3000
