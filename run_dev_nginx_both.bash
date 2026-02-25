a3ce43797f173b486b122b94c0476ac0291dd60c
Tim Schäfer <tim.schaefer@ae.mpg.de>
2026-02-25 13:51:01 +0100

    FIX: fix links in admin interface

--- run_dev_nginx_both.bash
+++ run_dev_nginx_both.bash
@@ -3,14 +3,7 @@
 # Run this script to start nginx with the development configuration that serves both the frontend and backend.
 # Also runs the FastAPI backend on port 8000. Make sure to have nginx installed.
 #
-# To use this script, simply run it from the terminal. It will start nginx in the background and then run the FastAPI backend in the foreground.
-# However, you need to properly configure the frontend and backend paths in the audiorating settings files to match the nginx configuration:
-#
-# - In the frontend, in file frontend/settings/ar_settings.js, set the API_BASE_URL to http://localhost/ar_backend/api
-# - In the backend, in file backend/.env, make sure to:
-#        * set the AR_ROOTPATH to /ar_backend
-#        * set the AR_FRONTEND_URL to http://localhost:3000/rate/
-#        * make sure that AR_ALLOWED_ORIGINS to '["http://localhost:3000", "http://127.0.0.1:3000"]', the default.
+# To use this script, simply run it from the terminal. It will coyp cfg files and start nginx in the background and then run the FastAPI backend in the foreground.
 #
 # You can access the frontend at http://localhost:3000/rate/ and the backend API at http://localhost:3000/ar_backend/api.
 
