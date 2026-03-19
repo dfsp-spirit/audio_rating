2449971f5eb4e6312741dc4979d3ab11339d53a8
Tim Schaefer <ts+code@rcmd.org>
2026-03-19 19:19:36 +0100

    NEW: add frontend e2e tests

---
+++ frontend/tests/e2e/study_page.spec.js
@@ -0,0 +1,10 @@
+const { test, expect } = require('@playwright/test');
+
+test('study page loads and has begin-study button', async ({ page }) => {
+  await page.goto('http://localhost:3000/rate/study.html');
+
+  await expect(page).toHaveTitle(/Audio Rating Study/);
+
+  const beginButton = page.locator('#begin-study');
+  await expect(beginButton).toBeVisible();
+});
